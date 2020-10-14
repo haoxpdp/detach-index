7dedb36a10b37160de578653f9b43160ee3d8dd4
haoxpzzzz <haoxpdp@gmail.com>
2020-10-14 22:16:03 +0800

    init serach input

--- js/time.js
+++ js/time.js
@@ -1,16 +1,50 @@
 
 
 
-function initDate() {
+function initDate () {
     var initDate = new Date();
     var min = initDate.getMinutes();
-    if(initDate.getMinutes().toString().length == 1){
-        min = "0"+min;
+    if (initDate.getMinutes().toString().length == 1) {
+        min = "0" + min;
     }
-    $("#time").text(initDate.getHours() +":" + min)
+    $("#time").text(initDate.getHours() + ":" + min)
 
 }
 initDate();
+
+
+function searchToggle (obj, evt) {
+    var container = $(obj).closest('.search-wrapper');
+
+    if (!container.hasClass('active')) {
+        container.addClass('active');
+        evt.preventDefault();
+    }
+    else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0) {
+        container.removeClass('active');
+        // clear input
+        container.find('.search-input').val('');
+        // clear and hide result container when we press close
+        container.find('.result-container').fadeOut(100, function () { $(this).empty(); });
+    }
+}
+
+function submitFn (obj, evt) {
+    value = $(obj).find('.search-input').val().trim();
+
+    _html = "Yup yup! Your search text sounds like this: ";
+    if (!value.length) {
+        _html = "Yup yup! Add some text friend :D";
+    }
+    else {
+        _html += "<b>" + value + "</b>";
+    }
+
+    $(obj).find('.result-container').html('<span>' + _html + '</span>');
+    $(obj).find('.result-container').fadeIn(100);
+
+    evt.preventDefault();
+}
 
 $(document).ready(function () {
     setInterval(() => {
@@ -22,8 +56,8 @@ $(document).ready(function () {
         type: "get",
         url: "http://localhost:9045/test",
         success: function (response) {
-            console.log(response)            
+            console.log(response)
         }
     });
-    
+
 });
