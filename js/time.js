0bb1e0d9d4058d5e52eba8194e155bd1eff6b239
haoxpzzzz <haoxpdp@gmail.com>
2020-10-16 07:52:07 +0800

    add

--- js/time.js
+++ js/time.js
@@ -1,7 +1,4 @@
-
-
-
-function initDate () {
+function initDate() {
     var initDate = new Date();
     var min = initDate.getMinutes();
     if (initDate.getMinutes().toString().length == 1) {
@@ -10,41 +7,8 @@ function initDate () {
     $("#time").text(initDate.getHours() + ":" + min)
 
 }
+
 initDate();
-
-
-function searchToggle (obj, evt) {
-    var container = $(obj).closest('.search-wrapper');
-
-    if (!container.hasClass('active')) {
-        container.addClass('active');
-        evt.preventDefault();
-    }
-    else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0) {
-        container.removeClass('active');
-        // clear input
-        container.find('.search-input').val('');
-        // clear and hide result container when we press close
-        container.find('.result-container').fadeOut(100, function () { $(this).empty(); });
-    }
-}
-
-function submitFn (obj, evt) {
-    value = $(obj).find('.search-input').val().trim();
-
-    _html = "Yup yup! Your search text sounds like this: ";
-    if (!value.length) {
-        _html = "Yup yup! Add some text friend :D";
-    }
-    else {
-        _html += "<b>" + value + "</b>";
-    }
-
-    $(obj).find('.result-container').html('<span>' + _html + '</span>');
-    $(obj).find('.result-container').fadeIn(100);
-
-    evt.preventDefault();
-}
 
 $(document).ready(function () {
     setInterval(() => {
@@ -57,7 +21,7 @@ $(".search-icon").click(function (e) {
     e.preventDefault();
     var url = "https://www.baidu.com/s?ie=utf-8&word=" + $(".search-input").val();
     window.open(url)
-    window.location.href=url;
+    window.location.href = url;
     $(".search-input").val("");
 });
 
@@ -76,7 +40,7 @@ $("body").keypress(function (e) {
     // 快捷键
 
     // S，搜索
-    if (e.keyCode == 83 && $(".search-input").is(":focus") == false) {
+    if (e.keyCode === 83 && $(".search-input").is(":focus") === false) {
         $(".search-input").focus();
 
     }
@@ -84,8 +48,20 @@ $("body").keypress(function (e) {
 
 });
 
+var showSettings = false;
+
 $(".setting").click(function () {
-    if ($(".setting-container").css("display") == 'none') {
-        $(".setting-container").css("display", 'block')
+    if (!showSettings) {
+        $(".setting-container").fadeIn()
+        showSettings = true;
+    } else if (showSettings) {
+        $(".setting-container").fadeOut(100)
+        showSettings = false;
     }
 });
+
+
+function togglePopMenu(){
+
+
+}
