0fa46307423ec4cf7e8025eedf23170ee1229ed0
haoxp <haoxpdp@gmail.com>
2020-10-16 18:37:41 +0800

    add .

--- js/time.js
+++ js/time.js
@@ -21,41 +21,41 @@ $(".search-icon").click(function (e) {
     e.preventDefault();
     var url = "https://www.baidu.com/s?ie=utf-8&word=" + $(".search-input").val();
     window.open(url)
-    window.location.href = url;
-    $(".search-input").val("");
 });
 
 $("body").keypress(function (e) {
-    e.preventDefault();
 
     console.log(e.keyCode)
     if (e.which == 13 && $(".search-input").is(":focus")) {
         var url = "https://www.baidu.com/s?ie=utf-8&word=" + $(".search-input").val();
 
         window.location.href = url;
-        // $(".search-input").val("");
-        // window.open(url)
+        e.preventDefault();
     }
-
-    // 快捷键
-
-    // S，搜索
-    if (e.keyCode === 83 && $(".search-input").is(":focus") === false) {
-        $(".search-input").focus();
-
-    }
-
 
 });
+
+
+$("body").click(function (e){
+    let settingContainer = document.getElementById("setting-container")
+    let settings = document.getElementsByClassName("setting");
+    if(settingContainer.contains(e.target) || settings[0].contains(e.target)){
+        console.log("inner")
+    }else{
+        $("#setting-container").fadeOut(100)
+        showSettings = false;
+    }
+    console.log(e.target)
+})
 
 var showSettings = false;
 
 $(".setting").click(function () {
     if (!showSettings) {
-        $(".setting-container").fadeIn()
+        $("#setting-container").fadeIn()
         showSettings = true;
     } else if (showSettings) {
-        $(".setting-container").fadeOut(100)
+        $("#setting-container").fadeOut(100)
         showSettings = false;
     }
 });
